ed7311159ef6bc7160c335166007ba51c8500043
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-07-14 17:38:48 -0300

    reactotron installed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Jesus loves you!</h1>
-      </div>
-    );
-  }
-}
+import './config/reactotron';
+
+import Home from './pages/home';
+
+const App = () => <Home />;
 
 export default App;
