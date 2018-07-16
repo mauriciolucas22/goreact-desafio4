c08aea8d50dd7bb9a464204805a0b90eb79f757c
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-07-16 20:28:43 -0300

    ProductDetails estilized

--- src/components/ProductDetails/index.js
+++ src/components/ProductDetails/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // local styles
-import { Container } from './styles';
+import { Container, Content, Details } from './styles';
 
 // global Components
 import Header from '../Header';
@@ -9,7 +9,15 @@ import Header from '../Header';
 const ProductDetails = () => (
   <Container>
     <Header />
-    <h1>Product Details</h1>
+    <Content>
+      <img src="https://t-static.dafiti.com.br/czCvp3wBNPfehf7omYZfJacnxPY=/fit-in/427x620/dafitistatic-a.akamaihd.net%2fp%2fquiksilver-camiseta-quiksilver-hyperas-preta-8710-7136243-1-product.jpg" alt="product" />
+      <Details>
+        <strong>Camiseta 1</strong>
+        <small>Element</small>
+        <p>R$ 50,00</p>
+        <button type="button" onClick={() => {}}>Adicionar ao carrinho</button>
+      </Details>
+    </Content>
   </Container>
 );
 
--- src/components/ProductDetails/styles.js
+++ src/components/ProductDetails/styles.js
@@ -4,3 +4,55 @@ export const Container = styled.div`
   height: 100%
   width: 1200px;
 `;
+
+export const Content = styled.div`
+  margin-top: 20px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  img {
+    height: 500px;
+    width: 500px;
+    margin: 10px 10px 10px 10px;
+  }
+`;
+
+
+export const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  margin-left: 20px;
+
+  strong {
+    font-size: 50px;
+    font-weight: bold;
+    color: #000000;
+  }
+
+  small {
+    font-size: 14px;
+    color: #eee;
+    margin-top: 10px;
+    color: #666666
+  }
+
+  p {
+    margin-top: 100px;
+    color: #37BEA9;
+    font-size: 50px;
+    font-weight: bold;
+  }
+
+  button {
+    border: 0;
+    color: #fff;
+    background: #37BEA9;
+    width: 30%;
+    height: 30px;
+    margin-top: 20px;
+  }
+`;
