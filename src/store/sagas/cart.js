fa3e094fad8a8b26eccc31fac8596d23aa0294c6
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-07-23 20:05:11 -0300

    falta arrumar cart

--- src/store/sagas/cart.js
+++ src/store/sagas/cart.js
@@ -1,13 +1,14 @@
-import { call, put } from 'redux-saga/effects';
+import { select, put } from 'redux-saga/effects';
 import api from '../../services/api';
 
 import { Actions as CartActions } from '../ducks/cart';
 
 export function* getCart() {
   try {
-    const response = yield call(api.get, '/products');
-
-    yield put(CartActions.addToCart(response.data));
+    // const response = yield call(api.get, '/products');
+    const response = yield select(state => state.cart.data);
+    console.tron.log(response);
+    // yield put(CartActions.addToCart(response));
   } catch (err) {
     console.tron.log('Error in get Cart');
   }
