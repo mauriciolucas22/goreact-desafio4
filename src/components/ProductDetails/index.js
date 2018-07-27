e23595c2c54cf95ed6f2a209fe5ff0c4ea851b26
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-07-27 19:12:09 -0300

    error in ProductDetails, not loading object of redux

--- src/components/ProductDetails/index.js
+++ src/components/ProductDetails/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -15,22 +15,27 @@ class ProductDetails extends Component {
     this.props.getProduct(this.props.match.params.id);
   }
 
+  renderDetails = productSelected => (
+    <Fragment>
+      <Header />
+      <p>{productSelected.name}</p>
+      <Content>
+        <img src="https://t-static.dafiti.com.br/czCvp3wBNPfehf7omYZfJacnxPY=/fit-in/427x620/dafitistatic-a.akamaihd.net%2fp%2fquiksilver-camiseta-quiksilver-hyperas-preta-8710-7136243-1-product.jpg" alt="product" />
+        <Details>
+          <strong>Camiseta Hyperas Preta</strong>
+          <small>Quiksilver</small>
+          <p>R$ 49.99</p>
+          <button type="button" onClick={() => {}}>Adicionar ao carrinho</button>
+        </Details>
+      </Content>
+    </Fragment>
+  );
+
   render() {
-    const { productSelected, loading } = this.props.database;
+    const { productSelected } = this.props.database;
     return (
       <Container>
-        <Header />
-        { loading ? <i className="fa fa-spinner" /> : (
-          <Content>
-            <img src={productSelected.image} alt="product" />
-            <Details>
-              <strong>Camiseta Hyperas Preta</strong>
-              <small>Quiksilver</small>
-              <p>R$ 49.99</p>
-              <button type="button" onClick={() => {}}>Adicionar ao carrinho</button>
-            </Details>
-          </Content>
-        ) }
+        { this.renderDetails(productSelected) }
       </Container>
     );
   }
